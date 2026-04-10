e781e3d2a9cc1b2a5fbc82904f8525b7f7df637a
Md Shahrukh Salim <mdshahrukhsalim191812@gmail.com>
2026-04-10 16:20:29 +0530

    Set Bal Bharti School Template

--- src/components/templates/BalBhartiSchool.tsx
+++ src/components/templates/BalBhartiSchool.tsx
@@ -36,7 +36,7 @@ export default function Template7({
                 <img
                     src={image}
                     alt="student"
-                    className="absolute top-[102px] left-[85px] w-[98px] h-[120px] object-cover rounded-md border"
+                    className="absolute top-[109.9px] left-[130px] w-[115px] h-[146.5px] object-cover"
                 />
             )}
 
@@ -44,31 +44,34 @@ export default function Template7({
                 <img
                     src={logo}
                     alt="logo"
-                    className="absolute top-[140px] left-[20px] w-[60px] h-[60px] object-contain"
+                    className="absolute top-[94.5px] left-[26px] w-[74px] h-[74px] object-contain"
                 />
             )}
 
-            <div className="absolute top-[229px] left-1/2 -translate-x-1/2 w-[200px] text-center text-white font-bold text-sm py-1 rounded">
+            <div className="absolute top-[270px] left-[20px] text-[13px] font-bold text-indigo-900 leading-tight space-y-[2px]" style={{ fontFamily: "Arial Narrow, Arial, sans-serif" }}>
+                <p className="relative top-[-54px] right-[2px]">Class : <span className="text-black">{student.class || "-"}</span> </p>
+                <p className="relative top-[-58px] right-[2px]">Sec : {student.sec || "-"}</p>
+                <p className="relative top-[-61px] right-[2px]">Roll No : {student.roll || "-"}</p>
+                <p className="relative top-[-65px] right-[2px]">D.O.B : {formatDate(student.dob) || "-"}</p>
+            </div>
+
+            <div className="absolute top-[276px] left-1/2 -translate-x-1/2 w-[220px] text-center text-black font-bold text-[18px] py-1 rounded">
                 {student.name || "----"}
             </div>
 
-            <div className="absolute top-[270px] left-[20px] text-[13px] font-bold text-blue-800 leading-tight space-y-[2px]">
-                <p>Class : {student.class || "-"}</p>
-                <p>D.O.B : {formatDate(student.dob) || "-"}</p>
-                <p>F. Name : {student.father || "-"}</p>
-                <p>Mobile : {student.phone || "-"}</p>
-                <p>Address : {student.address || "-"}</p>
-            </div>
-
-            <div className="absolute top-[290px] right-[20px] text-[13px] font-bold text-blue-800">
-                <p>Roll No : {student.roll || "-"}</p>
+            <div className="absolute top-[290px] left-[20px] text-[13px] font-bold text-indigo-900" style={{fontFamily:"Arial Narrow, Arial, sans serif"}}>
+                <p className="relative bottom-[-20.5px] right-[2.9px]">Admn. No : {student.admn || "-"}</p>
+                <p className="relative bottom-[-16.5px] right-[2.9px]">F. Name : {student.father || "-"}</p>
+                <p className="relative bottom-[-11.5px] right-[2.9px]">M. Name : {student.mother || "-"}</p>
+                <p className="relative bottom-[-7px] right-[2.9px]">Ph. No. : {student.phone || "-"}</p>
+                <p className="relative bottom-[-2px] right-[2.9px] leading-tight">Address : {student.address || "-"}</p>
             </div>
 
             {signature && (
                 <img
                     src={signature}
                     alt="signature"
-                    className="absolute bottom-[20px] right-[20px] w-[80px]"
+                    className="absolute bottom-[30px] right-[20px] w-[60px]"
                 />
             )}
 
