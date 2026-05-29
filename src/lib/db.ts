370defd54637d003bb6f8232acc2160336abd0ee
Md Shahrukh Salim <mdshahrukhsalim191812@gmail.com>
2026-05-29 10:25:10 +0530

    db change

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,35 @@
 import mongoose from "mongoose";
 
+const MONGODB_URI =
+    process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+    throw new Error(
+        "Please define MONGODB_URI"
+    );
+}
+
 export const connectDB = async () => {
+
     try {
-        if (mongoose.connection.readyState === 1) {
+
+        if (
+            mongoose.connection.readyState >= 1
+        ) {
             return;
         }
 
-        await mongoose.connect(process.env.MONGODB_URI as string);
+        await mongoose.connect(
+            MONGODB_URI
+        );
+
     } catch (error) {
-        console.error("MongoDB Error ❌", error);
+
+        console.error(
+            "MongoDB Error ❌",
+            error
+        );
+
+        throw error;
     }
 };
